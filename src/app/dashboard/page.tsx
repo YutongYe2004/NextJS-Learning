7b754840ddeaf2c2082ac4a4275246085908dc3c
YutongYe2004 <yeyutong2021@hotmail.com>
2025-06-04 04:01:52 -0400

    01:Routing

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>
+      'dashboard' routing to dashboard/page.tsx.<br />
+      This is the dashboard page.<br />
+      <br />
+    </div>
+  )
+}
