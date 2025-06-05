cbe71dd0aef80decc9aad74872139a94625b2493
YutongYe2004 <yeyutong2021@hotmail.com>
2025-06-05 05:30:00 -0400

    Increment Button

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-import React from 'react'
-import './globals.css'
+"use client";
+import React from 'react';
+import Link from 'next/link';
+import './globals.css';
+import { useState } from 'react';
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const [count, setCount] = useState(0)
   return (
-  <div className = "border-2 border-dashed border-black p-4">
-    <h2>Dashboard Layout</h2>
-    <main>{children}</main>
+  <div className = "border-2 border-dashed border-black p-4 w-1/2">
+    <h2>Dashboard Layout {count} </h2>
+    <button onClick={() => setCount(count + 1)} className = "bg-blue-500 text-white p-2 rounded my-2">Increment</button>
+    <div className = "flex gap-4 font-bold text-lg text-blue-500"> 
+      <Link href = "/dashboard/settings">Settings</Link>
+    </div>
+    {children}
   </div>
   )
 }
